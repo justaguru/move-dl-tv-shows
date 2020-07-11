2eb5c5d992477ed168b445f8449a44adc831bd64
John Gormley <gamer+github@gormleys.org>
2020-07-11 09:54:48 -0400

    Select NAS or Local sources automatically

--- move_dl_tv_shows.sh
+++ move_dl_tv_shows.sh
@@ -1,17 +1,34 @@
 # move downloaded shows to a Plex folder
 #find . -type f -iname "*john.oliver*" \| grep -i 'avi$\|mkv$\|mp4$'
 
-#SOURCE="/mnt/x/video/tv.test/"
-SOURCE="/mnt/x/video/tv_shows/"
-DESTINATION="/mnt/x/video/tv.streams/"
-deleteEXT=("nfo" "exe" "txt")
+# set source for local testing and running on NAS, then select based on which env running
+NASSOURCE="/volume1/Media/video/tv.incoming/"
+NASDESTINATION="/volume1/Media/video/tv.streams/"
+LOCALSOURCE="/mnt/x/video/tv.incoming/"
+LOCALDESTINATION="/mnt/x/video/tv.streams/"
+if [[ -d $NASSOURCE ]]
+then
+	SOURCE="$NASSOURCE"
+	DESTINATION="$NASDESTINATION"
+elif [[ -d $LOCALSOURCE ]]
+then
+	SOURCE="$LOCALSOURCE"
+	DESTINATION="$LOCALDESTINATION"
+else
+	echo "CANNOT find any directories"
+	exit -1
+fi
 
-# shows... [Folder]=search_string
+deleteEXT=("nfo" "exe" "txt")
+doCreatDirs="createDirs.do"
+logfile="activity.log"
+
 declare -A shows=(
 ["60_Minutes"]="*60.Minutes*"
 ["Broke"]="*Broke*"
 ["DCs_Legends_of_Tomorrow"]="*Legends.of.Tomorrow*"
 ["Doom_Patrol"]="*Doom.Patrol*"
+["Harley_Quinn"]="*Harley.Quinn*"
 ["Holey_Moley"]="*Holey.Moley*"
 ["Hollywood_Game_Night"]="*Hollywood.Game.Night*"
 ["Last_Week_Tonight_with_John_Oliver"]="*Last.Week.Tonight*"
@@ -22,6 +39,14 @@ declare -A shows=(
 ["The_Late_Show_with_Stephen_Colbert"]="*Late.Show.Colbert*"
 )
 
+
+# should not have to modify anything below here
+LOGOUT="$SOURCE$logfile"
+DOOUT="$SOURCE$doCreatDirs"
+RAN=$(date '+%Y:%m:%d:%H:%M')
+echo "$RAN" >> "$LOGOUT"
+
+# shows... [Folder]=search_string
 function move_if_dir_exists () {
 	echo "In function..."
 	file=$1
@@ -33,12 +58,17 @@ function move_if_dir_exists () {
 		echo "Move $file"
 		echo "...... to: $dest"
 		mv -v "$file" "$dest"
+		echo "MOVED $file --> $dest" >> "$LOGOUT"
 	else
 		echo "MISSING DIR: $dest"
+		echo "Create Dir: $dest" >> "$DOOUT"
 	fi
 }
 
 export -f move_if_dir_exists
+
+# Remove todo list if exists
+[ -f "$DOOUT" ] && rm "$DOOUT"
 
 for key in ${!shows[@]}; do 
 	searchFor=${shows[$key]}; 
@@ -47,7 +77,7 @@ for key in ${!shows[@]}; do
 	#echo $key = ${shows[$key]}; 
 done
 
-echo "Delete empty directories..."
+echo "Delete extra files and empty directories..."
 # remove non-video files
 find $SOURCE -type f -iname "*.txt" -o -iname "*.exe" -o -iname "*.nfo" | xargs -I {} -n 1 rm -v "{}"
 
